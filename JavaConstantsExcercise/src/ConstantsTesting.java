7345da245b79d698a35c29c864cda0592559c7e2
Ray <mkeller1991@gmail.com>
2018-02-12 18:46:24 -0600

    adding constants exercise

---
+++ JavaConstantsExcercise/src/ConstantsTesting.java
@@ -0,0 +1,17 @@
+
+public class ConstantsTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final int MY_CONSTANT = 7; //declaration and initialization 
+		int myVariable = 7;
+		
+		myVariable += 1; //assignment
+
+				
+		
+		
+	}
+
+}
